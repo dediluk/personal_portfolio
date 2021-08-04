7cecc246617fae86f7b43af5f39da8d986cc952b
Denis Iliukovich <dediluk@gmail.com>
2021-08-04 16:07:52 +0300

    added bootstrap, post's detail page

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
+
 
 def all_blogs(request):
     posts = Blog.objects.order_by('-date')
-    return render(request, 'blog/home.html', {'posts': posts})
+    return render(request, 'blog/all_blogs.html', {'posts': posts})
+
+
+def detail(request, blog_id):
+    print('=======================', blog_id)
+    post = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detail.html', {'post': post})
