f2dd5de944c3190a948cae31e2f93af89495d0ca
Denis Iliukovich <45496175+dediluk@users.noreply.github.com>
2021-09-01 15:01:53 +0300

    Update number_of_projects.js
    fixed jumping number of projects text

--- portfolio/static/js/number_of_projects.js
+++ portfolio/static/js/number_of_projects.js
@@ -1,15 +1,14 @@
-let number_of_projects = document.querySelector('.number_of_projects_text').getAttribute('data-number'),
-    number_of_projects_h2 = document.querySelector('.number_of_projects_text'),
+let number_of_projects_h2 = document.querySelector('.just_p'),
+    number_of_projects = number_of_projects_h2.getAttribute('data-number'),
     start = 0;
 
 function increasing() {
     if (start == number_of_projects) {
         clearInterval(interval);
     }
-    console.log(number_of_projects_h2.textContent.slice(1,-1) + start)
-    number_of_projects_h2.textContent = number_of_projects_h2.textContent.slice(1,-1) + start
+    number_of_projects_h2.textContent = start
     start += 1;
 }
 
-let interval = setInterval(increasing, 260);
+let interval = setInterval(increasing, 290);
 
